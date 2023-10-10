24bb483802dd74c53a4f5fca7412b23f5521ba3b
anatolguidea <guidea.anatol@gmail.com>
2023-10-10 15:28:24 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class SortareBubble {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] numere = {5, 2, 9, 1, 5, 6};
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Lista initiala:");
+        afisareLista(numere);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        bubbleSort(numere);
+
+        System.out.println("Lista sortata:");
+        afisareLista(numere);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean schimbare;
+
+        do {
+            schimbare = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    // Schimba elementele daca sunt in ordine inversa
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    schimbare = true;
+                }
+            }
+        } while (schimbare);
+    }
+
+    public static void afisareLista(int[] arr) {
+        for (int numar : arr) {
+            System.out.print(numar + " ");
         }
+        System.out.println();
     }
 }
